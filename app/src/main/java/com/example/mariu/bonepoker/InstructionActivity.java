1e6237c4f5c9bcaf40eaab788d01a7e7f58cf8bc
MariuszGarbinski <mariuszgarbinski@gmail.com>
2017-10-31 19:22:11 +0100

    zakończenie pracy z instrukcją

--- app/src/main/java/com/example/mariu/bonepoker/InstructionActivity.java
+++ app/src/main/java/com/example/mariu/bonepoker/InstructionActivity.java
@@ -12,6 +12,7 @@ public class InstructionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_instruction);
 
         TextView instruction = (TextView) findViewById(R.id.txtInstruction);
+        TextView instruction1 = (TextView) findViewById(R.id.txtInstruction1);
         instruction.setText("1. Cel gry:\n\n" +
                 "Celem gry jest uzyskanie jak największej ilości punktów. Gracz " +
                 "otrzymuje punkty za wyrzucenie odpowiednich oczek na kościach do gry. " +
@@ -22,7 +23,14 @@ public class InstructionActivity extends AppCompatActivity {
                 "Rzutu koścmi dokonuje się poprzez potrząśnięcie telefonem. Aby zablokować kości, " +
                 "które będą niezmienne podczas kolejnych rzutów, gracz musi przesunąć suwak pod kością. " +
                 " Przesuwając ponownie suwak gracz może oblokować kość, która ponownie będzie rzucana.\n\n" +
-                "3. Punktacja\n" +
-                "");
+                "3. Punktacja\n");
+
+        instruction1.setText("4. Zapis i odczyt wyników:\n\n" +
+                "Po ukończeniu gry (15 rund), przycisk 'ZAPISZ WYNIK' zostanie aktywowany, " +
+                "który podczas gry jest nieaktywny. Po jego kliknięciu wynik wraz z datą " +
+                "i godziną zakończenia zostanie zapisany w bazie wyników. " +
+                "W każdej chwili można sprawdzić uzyskane wyniki poprzez kliknięcie przycisku " +
+                "'OSIĄGNIĘCIA', znajdujący się na panelu głównym gry. Zotsną wyświetlone " +
+                "wszystkie uzyskane wyniki gier.");
     }
 }
